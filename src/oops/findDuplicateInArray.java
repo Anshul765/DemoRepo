41fd7f4795a32d1e99f161b760750bd9d4ecbe37
Anshul Thakral <anshul_thakral@external.mckinsey.com>
2022-03-16 01:46:29 +0530

    added one class

--- src/oops/findDuplicateInArray.java
+++ src/oops/findDuplicateInArray.java
@@ -14,8 +14,7 @@ public class findDuplicateInArray {
 			}
 			
 		}
-		
- 
+
 	}
 
 }
---
+++ src/oops/testlogic.java
@@ -0,0 +1,9 @@
+package oops;
+
+public class testlogic {
+
+	public static void main(String[] args) {
+			System.out.println("logic is in progress");
+	}
+
+}
